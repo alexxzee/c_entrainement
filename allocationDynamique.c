d763a3a3b010ab6f5e519efe4e9d4d5ea20e6aaa
root <root@localhost.localdomain>
2019-08-12 17:52:17 +0200

    save

---
+++ allocationDynamique.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <string.h>
+#include <stdbool.h>
+
+typedef struct Coordonnees Coordonnees;
+struct Coordonnees{
+  int x;
+  // int y;
+};
+
+int* memoireAllouee = NULL;
+memoireAllouee = malloc(sizeof(int));
+
+void main(){
+  // printf( "Coordonnees : %d octets \n", sizeof(Coordonnees) );
+}
+https://openclassrooms.com/fr/courses/19980-apprenez-a-programmer-en-c/16595-lallocation-dynamique#/id/r-2190224
